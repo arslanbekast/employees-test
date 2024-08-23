efe50b787f087ae9a16c6fb4a39f2c45dce01c49
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-23 21:19:43 +0300

    feat: add Provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './app/styles/index.scss'
+import { Provider } from 'react-redux'
+import { store } from '@/app/store'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 )
