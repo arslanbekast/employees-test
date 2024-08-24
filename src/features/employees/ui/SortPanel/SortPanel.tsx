1688fbb989ba16fa823fd9ff24f34bcc22c4b8ed
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-24 11:49:12 +0300

    feat: create SortPanel component

---
+++ src/features/employees/ui/SortPanel/SortPanel.tsx
@@ -0,0 +1,35 @@
+import s from './SortPanel.module.scss'
+import { useAppDispatch } from '@/common/hooks/useAppDispatch'
+import { setSort, Sort } from '@/features/employees/model/employeesSlice'
+import { useSelector } from 'react-redux'
+import { selectSort } from '@/features/employees/model/employees.selectors'
+
+export const SortPanel = () => {
+  const dispatch = useAppDispatch()
+  const sort = useSelector(selectSort)
+
+  const handleSortChange = (sortValue: Sort) => {
+    const newValue = sortValue === sort ? null : sortValue
+    dispatch(setSort(newValue))
+  }
+
+  return (
+    <div className={s.sortPanel}>
+      <span>Сортировать:</span>
+      <div className={s.btnsWrapper}>
+        <button
+          onClick={() => handleSortChange('name')}
+          className={sort === 'name' ? s.active : ''}
+        >
+          по Имени
+        </button>
+        <button
+          onClick={() => handleSortChange('birthday')}
+          className={sort === 'birthday' ? s.active : ''}
+        >
+          по Дате
+        </button>
+      </div>
+    </div>
+  )
+}
