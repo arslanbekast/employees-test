50e6f5b9fbe4299e01cde313d12585f2e2608a67
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-24 11:49:50 +0300

    feat: change App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from 'react'
+import { useAppDispatch } from '@/common/hooks/useAppDispatch'
+import { Employees } from '@/features/employees/ui/Employees'
+import { setEmployees } from '@/features/employees/model/employeesSlice'
+import s from './App.module.scss'
+import { AppInfo } from '@/app/AppInfo/AppInfo'
+
 export function App() {
-  return <div>Hello</div>
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    fetch('/employees.json')
+      .then(response => response.json())
+      .then(jsonData => dispatch(setEmployees(jsonData)))
+  }, [])
+
+  return (
+    <div className={s.app}>
+      <AppInfo />
+      <Employees />
+    </div>
+  )
 }
