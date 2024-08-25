caec0c4937ab5a1d38bf65d83601d2f2aba68e7c
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-25 14:52:06 +0300

    refactor: change styles in ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,3 +1,10 @@
+import s from './ErrorPage.module.scss'
+
 export const ErrorPage = () => {
-  return <div>404. Такой страницы не существует.</div>
+  return (
+    <div className={s.errorPage}>
+      <span className={s.errorCode}>404</span>
+      <span className={s.errorText}>Такой страницы не существует</span>
+    </div>
+  )
 }
