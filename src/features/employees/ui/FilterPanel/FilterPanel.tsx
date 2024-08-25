da12f6fc992dcc8a49450b06a5f334fa3e5185be
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-25 12:01:41 +0300

    refactor: refactoring FilterPanel

--- src/features/employees/ui/FilterPanel/FilterPanel.tsx
+++ src/features/employees/ui/FilterPanel/FilterPanel.tsx
@@ -4,13 +4,22 @@ import { selectFilter } from '@/features/employees/model/employees.selectors'
 import { ChangeEvent } from 'react'
 import { setFilter } from '@/features/employees/model/employeesSlice'
 import { useAppDispatch } from '@/common/hooks/useAppDispatch'
+import { Select } from '@/common/components/Select/Select'
+import { Checkbox } from '@/common/components/Checkbox/Checkbox'
+
+const selectOptions = [
+  { id: 1, value: '', label: 'Все сотрудники' },
+  { id: 2, value: 'cook', label: 'Повар' },
+  { id: 3, value: 'waiter', label: 'Официант' },
+  { id: 4, value: 'driver', label: 'Водитель' },
+]
 
 export const FilterPanel = () => {
   const filter = useSelector(selectFilter)
   const dispatch = useAppDispatch()
 
   const handleRoleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setFilter({ ...filter, role: e.target.value || null }))
+    dispatch(setFilter({ ...filter, role: e.currentTarget.value || null }))
   }
 
   const handleArchiveChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -21,16 +30,8 @@ export const FilterPanel = () => {
     <div className={s.filterPanel}>
       <h2>Фильтр:</h2>
       <div className={s.filtersWrapper}>
-        <select value={filter.role || ''} onChange={handleRoleChange}>
-          <option value="">Все сотрудники</option>
-          <option value="cook">Повар</option>
-          <option value="waiter">Официант</option>
-          <option value="driver">Водитель</option>
-        </select>
-        <div className={s.isArchiveWrapper}>
-          <input id={'isArhive'} type={'checkbox'} onChange={handleArchiveChange} />
-          <label htmlFor={'isArhive'}>В архиве</label>
-        </div>
+        <Select value={filter.role || ''} options={selectOptions} onChange={handleRoleChange} />
+        <Checkbox label={'В архиве'} onChange={handleArchiveChange} />
       </div>
     </div>
   )
